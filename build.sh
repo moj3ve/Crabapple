0fdcadc96f99b0c78dc3aa80784d02b329d26966
aspen <luxx4x@protonmail.com>
2020-07-07 21:07:08 -0400

    Optimize release builds.

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 rm -rf out || true
 mkdir out
 cd src/crabapple-example
-cargo +nightly build --target=aarch64-apple-ios
+cargo +nightly build --target=aarch64-apple-ios --release
 cd ../..
 cp target/aarch64-apple-ios/debug/libcrabapple_example.dylib out/arm64.dylib
 cd src/crabapple-example
-cargo +nightly build --target=aarch64-apple-ios --features arm64e
+cargo +nightly build --target=aarch64-apple-ios --features arm64e --release
 cd ../..
 cp target/aarch64-apple-ios/debug/libcrabapple_example.dylib out/_arm64e.dylib
 res/arm64patcher out/_arm64e.dylib out/arm64e.dylib
